335a3d7bd5611796c4e5c8ad431c6b02d1388866
Menezes-A <analivia.fm4@gmail.com>
2021-03-22 20:33:00 -0300

    Atividade7

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { Text } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import FirebaseApp from './components/FirebesaApp'
+import FirebaseApp from './components/Atividade6/FirebesaApp'
+import Rotas from './components/Atividade7/src/Rotas'
 
 export class App extends Component {
   render() {
     return (
-      <FirebaseApp></FirebaseApp>
+      <SafeAreaProvider>
+      <Rotas></Rotas>
+      </SafeAreaProvider>
     )
   }
 }
